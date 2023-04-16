cecf3eb6126297d861b0074e45cf8aacfe63af27
Kenneth <kenlaidlaw@gmail.com>
2023-04-16 19:37:23 +0100

    First Draft

--- script.js
+++ script.js
@@ -10,20 +10,82 @@ function fixNav() {
 }
 
 // taking the active class of home when about is clicked
-const homeClick = document.getElementById('home_click');
-const aboutClick = document.getElementById('about_click');
+const homeClick = document.getElementById("home_click");
+const aboutClick = document.getElementById("about_click");
+const galleryClick = document.getElementById("gallery_click");
+const contactClick = document.getElementById("contact_click");
 
-aboutClick.addEventListener('click', aboutCurrent);
+aboutClick.addEventListener("click", aboutCurrent);
 
 function aboutCurrent() {
-  aboutClick.classList.add('current')
-  homeClick.classList.remove('current')
+  aboutClick.classList.add("current");
+  homeClick.classList.remove("current");
+  galleryClick.classList.remove("current");
+  contactClick.classList.remove("current");
 }
 
-homeClick.addEventListener('click', homeCurrent);
+homeClick.addEventListener("click", homeCurrent);
 
 function homeCurrent() {
-  aboutClick.classList.remove('current')
-  homeClick.classList.add('current')
+  aboutClick.classList.remove("current");
+  homeClick.classList.add("current");
+  galleryClick.classList.remove("current");
+  contactClick.classList.remove("current");
 }
 
+galleryClick.addEventListener("click", galleryCurrent);
+
+function galleryCurrent() {
+  aboutClick.classList.remove("current");
+  homeClick.classList.remove("current");
+  galleryClick.classList.add("current");
+  contactClick.classList.remove("current");
+}
+homeClick.addEventListener("click", contentCurrent);
+
+function contentCurrent() {
+  aboutClick.classList.remove("current");
+  homeClick.classList.remove("current");
+  galleryClick.classList.remove("current");
+  contactClick.classList.add("current");
+}
+
+// slide show
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slider-img");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slider-img");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
