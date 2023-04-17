bdc6d2404d99635027c189921f6b93e6828b7527
Kenneth <kenlaidlaw@gmail.com>
2023-04-17 09:58:26 +0100

    responsive fix

--- script.js
+++ script.js
@@ -9,47 +9,6 @@ function fixNav() {
   }
 }
 
-// taking the active class of home when about is clicked
-const homeClick = document.getElementById("home_click");
-const aboutClick = document.getElementById("about_click");
-const galleryClick = document.getElementById("gallery_click");
-const contactClick = document.getElementById("contact_click");
-
-aboutClick.addEventListener("click", aboutCurrent);
-
-function aboutCurrent() {
-  aboutClick.classList.add("current");
-  homeClick.classList.remove("current");
-  galleryClick.classList.remove("current");
-  contactClick.classList.remove("current");
-}
-
-homeClick.addEventListener("click", homeCurrent);
-
-function homeCurrent() {
-  aboutClick.classList.remove("current");
-  homeClick.classList.add("current");
-  galleryClick.classList.remove("current");
-  contactClick.classList.remove("current");
-}
-
-galleryClick.addEventListener("click", galleryCurrent);
-
-function galleryCurrent() {
-  aboutClick.classList.remove("current");
-  homeClick.classList.remove("current");
-  galleryClick.classList.add("current");
-  contactClick.classList.remove("current");
-}
-homeClick.addEventListener("click", contentCurrent);
-
-function contentCurrent() {
-  aboutClick.classList.remove("current");
-  homeClick.classList.remove("current");
-  galleryClick.classList.remove("current");
-  contactClick.classList.add("current");
-}
-
 // slide show
 var slideIndex = 1;
 showDivs(slideIndex);
